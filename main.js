6b70e73ad40db2f88f483545190318e54f8653d5
Kenneth Prado <Kennethprado45@gmail.com>
2015-04-29 12:08:56 -0700

    Week2 Assesment Finished.

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('.dan').click(function(){
+
+	alert("Hey you Clicked me!");
+
+	});
+
+});
+
+$(document).ready(function(){
+	$('#iwao').click(function(){
+	$('#iwao').hide();
+
+	});
+
+});
